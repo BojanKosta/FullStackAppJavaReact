7c8724be27d64080b01fe859bb28d7c653983393
Bojan Todorovic <bojankosta1989@gmail.com>
2020-01-12 22:18:28 +0100

    exceptions added and find by project identifier

--- ppmtool/src/main/java/com/bojankosta/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/bojankosta/ppmtool/services/ProjectService.java
@@ -1,6 +1,7 @@
 package com.bojankosta.ppmtool.services;
 
 import com.bojankosta.ppmtool.domain.Project;
+import com.bojankosta.ppmtool.exceptions.ProjectIdException;
 import com.bojankosta.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,26 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
-    public Project saveOrUpdate (Project project){
-        return projectRepository.save(project);
+    public Project saveOrUpdate(Project project) {
+
+        project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+        try {
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Cant add project with same project identifier. "
+                    + "Project with this projectId " + project.getProjectIdentifier() + " already exists.");
+        }
+    }
+
+    public Project findProject(String projectId) {
+
+        Project project = projectRepository.findByProjectIdentifier(projectId);
+
+        if (project == null) {
+            throw new ProjectIdException("Cant add project with same project identifier. "
+                    + "Project with this projectId " + project.getProjectIdentifier() + " does not exists.");
+        }
+
+        return project;
     }
 }
--- ppmtool/src/main/java/com/bojankosta/ppmtool/web/ProjectController.java
+++ ppmtool/src/main/java/com/bojankosta/ppmtool/web/ProjectController.java
@@ -2,33 +2,38 @@ package com.bojankosta.ppmtool.web;
 
 
 import com.bojankosta.ppmtool.domain.Project;
+import com.bojankosta.ppmtool.services.ErrorValidationService;
 import com.bojankosta.ppmtool.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping ("/api/project")
+@RequestMapping("/api/project")
 public class ProjectController {
 
     @Autowired
     private ProjectService projectService;
+    @Autowired
+    private ErrorValidationService errorValidationService;
 
     @PostMapping("")
     public ResponseEntity<?> saveOrUpdate(@Valid @RequestBody Project project, BindingResult result) {
 
-        if(result.hasErrors()){
-            return new ResponseEntity<String>("Invalid Project Object", HttpStatus.BAD_REQUEST);
-        }
+        ResponseEntity<?> error = errorValidationService.errorValidationService(result);
+        if (error != null) return error;
 
         Project project1 = projectService.saveOrUpdate(project);
         return new ResponseEntity<Project>(project1, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{projectId}")
+    public ResponseEntity <?> findProjectById (@PathVariable String projectId){
+        Project project = projectService.findProject(projectId);
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
 }
