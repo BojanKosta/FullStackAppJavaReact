315694e71b16a397cc2e308d9ba0d76b2bd495c9
Bojan Todorovic <bojankosta1989@gmail.com>
2020-01-14 20:24:31 +0100

    react part form added

---
+++ ppmtool-react-client/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+import Dashboard from './Components/Dashboard';
+import Header from './Components/Layout/Header';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from './Components/Project/AddProject';
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path = "/dashboard" component = { Dashboard }/>
+          <Route exact path = "/addProject" component = { AddProject } />
+        </div>
+      </Router>
+    );
+  }
+
+}
+
+export default App;
